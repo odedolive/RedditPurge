8e336685681aeaa9ee25e56efde5985a5b884374
Oded Olive <odedolive@gmail.com>
2019-12-28 22:06:59 -0400

    Simplified release script

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
-dotnet publish ./src/RedditPurgeApp -r win-x64 -c Release /p:PublishSingleFile=true
+dotnet publish ./src/RedditPurgeApp
 del release /q /f /s
 md release
 xcopy src\RedditPurgeApp\bin\Release\netcoreapp3.1\win-x64\chromedriver.exe release /y
